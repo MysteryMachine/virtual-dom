dcafac9387ae5f8dd98a4794f18107e6e91d7014
Sal Becker <physicsfu@gmail.com>
2016-12-10 22:54:26 -0500

    full features!

--- src/virtual_dom/core.cljs
+++ src/virtual_dom/core.cljs
@@ -1,20 +1,71 @@
 (ns virtual-dom.core
   (:require [reagent.core :as reagent :refer [atom]]
             [cljs.reader :refer [read-string]]
+            [clojure.string :as str]
             [virtual-dom.grammar :as grammar]
             [virtual-dom.reify :as vdom]))
 
 (enable-console-print!)
 
 (defonce state-atom
-  (atom {:code-str grammar/default-grammar-str
-         :grammar grammar/default-grammar
-         :* 0}))
+  (let [cookie js/document.cookie
+        code-str (if (not (empty? cookie))
+                   (-> cookie
+                       (str/split "a=")
+                       second
+                       (str/split ";")
+                       first)
+                   grammar/default-grammar-str)
+        grammar (try (read-string code-str)
+                     (catch js/Error e nil))
+        code-str (if grammar code-str grammar/default-grammar-str)
+        grammar (or grammar (read-string code-str))]
+    (atom {:code-str code-str
+           :grammar grammar
+           :* 0
+           :display :edit})))
 
 (defn redo []
   (swap!
    state-atom
    #(update % :* inc)))
+
+(defn edit []
+  (swap!
+   state-atom
+   #(assoc % :display :edit)))
+
+(def error-msg "There is an error in your grammar!")
+
+(defn play []
+  (swap!
+   state-atom
+   #(let [grammar (try
+                    (read-string (:code-str %))
+                    (catch js/Error e nil))]
+      (if grammar
+        (-> %
+            (assoc :display :play)
+            (assoc :grammar grammar)
+            (assoc :message nil))
+        (assoc % :message error-msg)))))
+
+(defn text-change [e]
+  (let [val (-> e .-target .-value)]
+    (swap! state-atom #(assoc % :code-str val))))
+
+(defn save []
+  (let [grammar (try
+                  (read-string (:code-str @state-atom))
+                  (catch js/Error e nil))]
+    (if grammar
+      (do
+        (set! js/document.cookie (str  "a=" (:code-str @state-atom)))
+        (swap! state-atom #(assoc % :message "Grammar saved!"))
+        (js/setTimeout
+         (fn [] (swap! state-atom #(assoc % :message nil)))
+         1500))
+      (swap! state-atom #(assoc % :message error-msg)))))
 
 (defn bullet [grammar command]
   [:li
@@ -26,25 +77,50 @@
   (let [{:keys [grammar code-str]} state]
    (into [:ul] (-> bullet (partial grammar) map) (:-> grammar))))
 
-(defn redo-button []
-  [:div.redo-outer
-   [:button.redo {:onClick redo} "Redo"]])
+(defn play-buttons []
+  [:div.buttons
+   [:button.bottom {:on-click redo} "Again"]
+   [:button.bottom {:on-click edit} "Edit"]])
 
-(defn virtual-dom []
+(defn edit-buttons [state]
+  (let [message (:message state)]
+    [:div
+     (when message [:p.message message])
+     [(if message :div.buttons.message :div.buttons)
+      [:button.bottom {:on-click save} "Save"]
+      [:button.bottom {:on-click play} "Play"]]]))
+
+(defmulti virtual-dom :display)
+
+(defmethod virtual-dom :edit [state]
+  [:div.virtual-dom-inner
+   [:div.code-container
+    [:textarea.code
+     {:value (:code-str state)
+      :on-change text-change}]]
+   [edit-buttons state]
+   [:p.readme
+    [:span "Need some help? "]
+    [:a {:href "https://github.com/MysteryMachine/virtual-dom/blob/master/README.md"
+         :target "_none"}
+     "Check out the README!"]]])
+
+(defmethod virtual-dom :play [state]
+  [:div.virtual-dom-inner
+   [bullets state]
+   [play-buttons]])
+
+(defn virtual-dom-app []
   (let [state @state-atom]
     [:div.virtual-dom
      [:h1 "Virtual DOM"]
-     [:h2 "A virtual dom for your cyber submissive needs."]
-     [:div.virtual-dom-inner
-      [:p "You! Virtual DOM commands you to"]
-      [bullets state]
-      [redo-button]]]))
+     [virtual-dom state]]))
 
 (def outer-id "virtual-dom-outer")
 
 (defn get-container [] (. js/document (getElementById outer-id)))
 
-(reagent/render-component [virtual-dom] (get-container))
+(reagent/render-component [virtual-dom-app] (get-container))
 
 (defn on-js-reload [])
 
--- src/virtual_dom/grammar.cljs
+++ src/virtual_dom/grammar.cljs
@@ -1,80 +1,19 @@
 (ns virtual-dom.grammar)
 
-(def default-grammar
-  '{:-> [:dress-up :locations :play-time :play-time :finish]
-    :dress-up ["Put on "
-               (:makeup
-                :underwear :underwear
-                :kinkywear :kinkywear
-                [:makeup " and " :underwear]
-                [:makeup " and " :kinkywear])
-               ". "]
-
-    :makeup ("your darkest lipstick"
-             "pretty pink lipstick")
-
-    :underwear (:undies :socks [:undies " and " :socks])
-
-    :undies ("your most revealing pair of undies"
-             "bright flamboyant undies"
-             "your fanciest pair of underwear")
-
-    :socks ("thigh highs"
-            "colorful socks")
-
-    :kinkywear ("nipple clamps"
-                "clothespins on a sensitive area"
-                "a gag"
-                "a blindfold"
-                "a blindfold and a gag"
-                "a gag and nipple clamps")
-
-    :locations ["Go to "
-                ("the bathroom"
-                 "your bedroom"
-                 "on a comfortable chair"
-                 "the shower")
-                ". "]
-
-    :play-time (:tease
-                [:tease " and " :substimulate]
-                :obey :obey
-                :punish
-                [:punish " and " :substimulate])
-
-    :roughness ("hard" "lightly")
-
-    :tease ["Tease your " :tease* "."]
-    :subtease ["tease your " :tease*]
-    :tease* [("anus" "genitals" "nipples" "breasts" "neck" "butt" "back")
-             " with "
-             ("ice" "something hot" "a feather" "your hands" "a shock wand")]
-
-    :obey (["On your knees with your your ass up and your head down, "
-            (:subspank :subtease) "."]
-           ["Sit with your legs spread out wide and "
-            (:substimulate :subtease) "."]
-           ["Watching yourself in a mirror "
-            (:subspank :subtease :substimulate) "."])
-
-    :substimulate ["play " :stimulate*]
-    :stimulate* ["with your genitals " ("very lightly" "rough and fast")]
-
-    :punish (:spank :pinch)
-
-    :spank ["Spank yourself " :spank*]
-    :subspank ["spank yourself " :spank*]
-    :spank* [:roughness " " (10 20 50) " times"]
-
-    :pinch ["Pinch your nipples " :roughness " for " (1 3 5) " minutes."]
-
-    :finish ["Begin to stimulate yourself more. You must play with yourself for "
-             (5 10 20)
-             " minutes " (:cum :edge)]
-
-    :cum ["before you are allowed to cum!"]
-
-    :edge ["but you are not allowed to cum! Wait "
-           (0 15 30 60) " minutes before trying this again."]})
-
-(def default-grammar-str (str default-grammar))
+(def default-grammar-str
+"{:-> [:pick-friend :cuddle-friend :eat]
+ :pick-friend [\"For this cuddle session, find \" :animal \".\"]
+ :animal (\"a soft stuffed bear\",
+          \"the biggest plush toy you have\")
+ :cuddle-friend [\"Cuddle your friend for \" :time :location \".\"]
+ :time [(5 10 20) \" minutes \"]
+ :location (\"on the couch\",
+            \"on your bed\",
+            \"wrapped in a blanket\")
+ :eat [\"Regain your energy by \" :food \".\"]
+ :food (:drink :snack)
+ :drink (\"having a cup of tea\"
+         \"having a cup of coffee\"
+         \"having a glass of water\")
+ :snack (\"having a cookie\"
+         \"having some carrots\")}")
